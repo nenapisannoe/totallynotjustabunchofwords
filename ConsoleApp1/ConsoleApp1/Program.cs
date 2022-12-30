4a71312117659a4bd3f4b0af0ab29a3ab370164c
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2022-12-30 15:45:06 +0300

    halo

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] lines = { "Oh, hey, you're back.", "Hyperion would like to remind you that there is only one thing worse than respawning, and that is not respawning.", "The Hyperion corporation suggests: Live! LIIIIIIIIVVVVVVVVE!", "The Hyperion corporation is sure none of that was your fault.",
+                                "Oh, it's you again.", "The Hyperion Corporation: We make your life. Period."};
+            Console.WriteLine(lines[rnd.Next(lines.Length)]);
+        }
+    }
+}
